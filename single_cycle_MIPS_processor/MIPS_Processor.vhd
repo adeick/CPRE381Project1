2d478929a8f947f723d754b12ce72d71e9c61634
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-03-11 00:59:54 -0600

    Instruction Slicing
    Additionally added the Control Unit

--- single_cycle_MIPS_processor/MIPS_Processor.vhd
+++ single_cycle_MIPS_processor/MIPS_Processor.vhd
@@ -57,12 +57,18 @@ architecture structure of MIPS_Processor is
   signal s_ALUSrc    : std_logic; -- TODO: use this signal as the final data memory data input
 
 --Added Signals  
-  signal s_RegOutReadData1, s_RegOutReadData2 : std_logic_vector(N-1 downto 0); 
+  signal s_RegOutReadData1 : std_logic_vector(N-1 downto 0);
+    --Data2 is named s_DMemData
   signal s_RegInReadData1,    s_RegInReadData2,            s_RegD: std_logic_vector(4 downto 0);
   --rs(instructions [25-21]), rt(instructions [20-16]),     rd (instructions [15-11])
   signal s_imm32 : std_logic_vector(31 downto 0);
   signal s_imm16 : std_logic_vector(15 downto 0); 
   signal s_immMuxOut : std_logic_vector(N-1 downto 0); --Output of Immediate Mux
+
+  signal s_opCode   : std_logic_vector(5 downto 0);--instruction bits[31-26] 
+  signal s_ALUOp    : std_logic_vector(5 downto 0);--instruction bits[31-26]
+  signal s_funcCode : std_logic_vector(5 downto 0);--instruction bits[5-0]
+  signal s_ALUCtrl  : std_logic_vector(11 downto 0);--Routes from ALU control to ALU
 
   component mem is
     generic(ADDR_WIDTH : integer;
@@ -77,6 +83,12 @@ architecture structure of MIPS_Processor is
 
   -- TODO: You may add any additional signals or components your implementation 
   --       requires below this comment
+  component control_unit is
+    port( i_opcode  	: in std_logic_vector(5 downto 0);
+	        i_funct	  	: in std_logic_vector(5 downto 0);
+	        o_Ctrl_Unt	: out std_logic_vector(11 downto 0));
+  end component;
+
   component regfile is 
   port(clk			: in std_logic;
       i_wA		: in std_logic_vector(4 downto 0); --Write Address
@@ -105,11 +117,16 @@ end component;
 
 begin
 
-  -- TODO: This is required to be your final input to your instruction memory. This provides a feasible method to externally load the memory module which means that the synthesis tool must assume it knows nothing about the values stored in the instruction memory. If this is not included, much, if not all of the design is optimized out because the synthesis tool will believe the memory to be all zeros.
+
+
   with iInstLd select
     s_IMemAddr <= s_NextInstAddr when '0',
       iInstAddr when others;
-
+  -- TODO: This is required to be your final input to your instruction memory. 
+  -- This provides a feasible method to externally load the memory module which means that the 
+  -- synthesis tool must assume it knows nothing about the values stored in the instruction memory. 
+  -- If this is not included, much, if not all of the design is optimized out because the synthesis 
+  -- tool will believe the memory to be all zeros.
 
   IMem: mem
     generic map(ADDR_WIDTH => 10,
@@ -129,6 +146,30 @@ begin
              we   => s_DMemWr,
              q    => s_DMemOut);
 
+  process(s_Inst) --snip the Instruction data into smaller parts
+  begin
+    for i in 0 to 15 loop
+			s_imm16(i) <= s_Inst(i); --bits[15-0] into Sign Extender
+		end loop;
+		for i in 0 to 5 loop --Control not Implemented Yet
+			s_funcCode(i) <= s_Inst(i); --bits[5-0] into ALU Control 
+		end loop;
+    for i in 11 to 15 loop
+			s_regD(i-11) <= s_Inst(i); --bits[11-15] into RegDstMux bits[4-0]
+		end loop;
+    for i in 16 to 20 loop
+			s_RegInReadData2(i-16) <= s_Inst(i); --bits[16-20] into RegDstMux and Register (bits[4-0])
+		end loop;
+    for i in 21 to 25 loop
+			s_RegInReadData1(i-21) <= s_Inst(i); --bits[25-21] into Register (bits[4-0])
+		end loop;
+    for i in 26 to 31 loop 
+			s_opCode(i-26) <= s_Inst(i); --bits[26-31] into Control Brick (bits[5-0])
+      s_ALUOp(i-26)  <= s_Inst(i); --bits[26-31] into Control Brick (bits[5-0])
+      --TODO Adjust this once Control Brick is implemented, opCode should change ALUOp
+		end loop;
+	end process;
+
   --RegFile: --
   registers: regfile 
   port map(clk			=> iCLK,--std_logic;
@@ -137,16 +178,16 @@ begin
 		i_wC	        	=> s_RegWr,--std_logic;
 		i_r1	        	=> s_RegInReadData1,--std_logic_vector(4 downto 0);
 		i_r2	        	=> s_RegInReadData2,--std_logic_vector(4 downto 0);
-		reset	        	=> s_,--std_logic;
+		reset	        	=> iRST,--std_logic;
     o_d1            => s_RegOutReadData1,-- std_logic_vector(31 downto 0);
-    o_d2            => s_RegOutReadData2);-- std_logic_vector(31 downto 0));
+    o_d2            => s_DMemData);-- std_logic_vector(31 downto 0));
 
   --AddSub: --
 
   immediateMux: mux2t1_N
   generic map(32 => N) -- Generic of type integer for input/output data width. Default value is 32.
   port map(i_S   => s_ALUSrc,
-       i_D0      => s_RegOutReadData2,
+       i_D0      => s_DMemData,
        i_D1      => s_imm32,
        o_O       => s_immMuxOut);
 
@@ -160,8 +201,13 @@ begin
   signExtender: extender
   port map( i_I     => s_imm16, --in std_logic_vector(15 downto 0);     -- Data value input
 	          i_C			=> '1', --in std_logic; --0 for zero, 1 for sign-extension
-            o_O     => s_imm32, --out std_logic_vector(31 downto 0));   -- Data value output);
+            o_O     => s_imm32); --out std_logic_vector(31 downto 0));   -- Data value output);
 
+  aluControl: control_unit
+  port map(i_opcode  	=> s_opCode, --in std_logic_vector(5 downto 0);
+          i_funct	  	=> s_funcCode, --in std_logic_vector(5 downto 0);
+          o_Ctrl_Unt	=> s_ALUCtrl); --out std_logic_vector(11 downto 0));
+        
   
   -- TODO: Ensure that s_Halt is connected to an output control signal produced from decoding the Halt instruction (Opcode: 01 0100)
   -- TODO: Ensure that s_Ovfl is connected to the overflow output of your ALU

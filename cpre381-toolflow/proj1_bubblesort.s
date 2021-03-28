48d8fac6377f601235cbe096237e6089553ba4a6
Rolf Anderson <asaprolfy@gmail.com>
2021-03-28 19:00:12 -0400

    bble

--- cpre381-toolflow/proj1_bubblesort.s
+++ cpre381-toolflow/proj1_bubblesort.s
@@ -1,12 +1,29 @@
+  #bubblesort tests for proc
+  #$a0 = a[]
+  #$a1 = n
+
+  addi    $t8, $a0        #ptr:   original pointer to a
+  addi    $t7, $zero, 0   #bool:  swapped
+  addi    $t6, $zero, 0   #int:   i
+
+whileloop:
+  slt   $t9, $t6, $a1
+  bne   $t9, 1, whiledone
+  
+
+
 swapif:
-	  lw    $t0, 0($a0)
-	  lw    $t1, 4($a0)
+	  lw    $t0, 0($t8)
+	  lw    $t1, 4($t8)
 	  slt   $t2, $t1, $t0
 	  beq   $t2, $zero, else
-	  sw    $t0, 4($a0)
-	  sw    $t1, 0($a0)
-	  addi  $v0, $zero, 1
-	  jr    $ra
+	  sw    $t0, 4($t8)
+	  sw    $t1, 0($a8)
+	  addi  $t7, $zero, 1
+	  jr    whileloop
 else:
-	  addi  $v0, $zero, 0
-	  jr    $ra
+	  addi  $t7, $zero, 0
+	  j     whileloop
+
+whiledone:
+    

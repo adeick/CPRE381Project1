ee40aa8c3e3772dd1ebf4990c7af4ea3848428d0
Rolf Anderson <asaprolfy@gmail.com>
2021-03-28 21:43:13 -0400

    moving to testing for bubble sort

--- cpre381-toolflow/proj1_bubblesort.s
+++ cpre381-toolflow/proj1_bubblesort.s
@@ -2,31 +2,30 @@
   #$a0 = a[]
   #$a1 = n
 
-  addi    $t8, $a0        #ptr:   original pointer to a
-  addi    $t7, $zero, 0   #bool:  swapped
-  addi    $t6, $zero, 0   #int:   i
+  addi    $t8, $a0, 0     ;ptr:     original pointer to a[]
+  addi    $t7, $zero, 0   ;bool:    swapped
+  addi    $t6, $zero, 0   ;int:     i
 
 whileloop:
   slt   $t9, $t6, $a1
   bne   $t9, 1, whiledone
+  j     swapif
   
-
-
 swapif:
-	  lw    $t0, 0($t8)
-	  lw    $t1, 4($t8)
-	  slt   $t2, $t1, $t0
-	  beq   $t2, $zero, else
-	  sw    $t0, 4($t8)
-	  sw    $t1, 0($a8)
-	  addi  $t7, $zero, 1
-	  jr    preloop
+	lw    $t0, 0($t8)             ;load a = a[i]
+	lw    $t1, 4($t8)             ;load b = a[i+1]
+	slt   $t2, $t1, $t0           ;if a[i+1] < a[i]
+	bne   $t2, $zero, else        ;if a[i+1] > a[i], go to else
+	sw    $t0, 4($t8)             ;a[i+1] = a[i]
+	sw    $t1, 0($a8)             ;a[i]   = a[i+1]
+	addi  $t7, $zero, 1           ;set swapped to true
+	jr    preloop                 ;go to preloop
 else:
-	  addi  $t7, $zero, 0
-	  j     preloop
+	addi  $t7, $zero, 0
+	j     preloop
 preloop:
-  addi    $t6, $t6, 1         #i++
-  addi    $t8, $t8, 1         #a[]++
-  j       whileloop
+  addi    $t6, $t6, 1           ;i++
+  addi    $t8, $t8, 1           ;a[]++
+  j       whileloop             ;go back to loop
 whiledone:
-    
+  

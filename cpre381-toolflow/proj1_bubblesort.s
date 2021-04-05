62ec51178c034200501ea2bf732760bab1d1441e
Rolf Anderson <asaprolfy@gmail.com>
2021-04-04 23:23:13 -0500

    Finished adding depth and examples to bubblesort

--- cpre381-toolflow/proj1_bubblesort.s
+++ cpre381-toolflow/proj1_bubblesort.s
@@ -4,12 +4,12 @@
   
 main:
   add	$a0, $gp, $zero
-  addi	$a1, $zero, 4
+  addi	$a1, $zero, 5
   addi	$t1, $zero, 1
   sw	$t1, 0($a0)
-  addi	$t1, $zero, 3
-  sw	$t1, 4($a0)
   addi	$t1, $zero, 2
+  sw	$t1, 4($a0)
+  addi	$t1, $zero, 3
   sw	$t1, 8($a0)
   addi	$t1, $zero, 4
   sw	$t1, 12($a0)
@@ -20,21 +20,22 @@ bubble:
   addi  $t8, $a0, 0     #ptr:     original pointer to a[]
   addi  $t7, $zero, 0   #bool:    swapped
   addi  $t6, $zero, 0   #int:     i
-  j	whileloop
 
 whileloop:
   slt   $t9, $t6, $a1
   bne   $t9, 1, whiledone
   
 swapif:
-	lw    $t0, 0($t8)             #load a = a[i]
-	lw    $t1, 4($t8)             #load b = a[i+1]
-	slt   $t2, $t1, $t0           #if a[i+1] < a[i]
-	bne   $t2, $zero, else        #if a[i+1] > a[i], go to else
-	sw    $t0, 4($t8)             #a[i+1] = a[i]
-	sw    $t1, 0($t8)             #a[i]   = a[i+1]
-	addi  $t7, $zero, 1           #set swapped to true
-	j     preloop                 #go to preloop
+	lw	$t0, 0($t8)             #load a = a[i]
+	addi	$t0, $t0, 0
+	lw	$t1, 4($t8)             #load b = a[i+1]
+	addi	$t1, $t1, 0
+	slt	$t2, $t1, $t0           #if a[i+1] < a[i]
+	bne	$t2, 1, else        	#if a[i+1] > a[i], go to else
+	sw	$t1, 0($t8)             #a[i]   = a[i+1]
+	sw	$t0, 4($t8)             #a[i+1] = a[i]
+	addi	$t7, $zero, 1           #set swapped to true
+	j	preloop                 #go to preloop
 else:
 	addi  $t7, $zero, 0
 	j     preloop
@@ -43,8 +44,8 @@ preloop:
   addi    $t8, $t8, 4           #a[]++
   j       whileloop             #go back to loop
 whiledone:
-  lw	$t1, 0($a0)
-  lw	$t2, 4($a0)
-  lw	$t3, 8($a0)
-  lw	$t4, 12($a0)
-  lw	$t5, 16($a0)
+  lw	$t0, 0($a0)
+  lw	$t1, 4($a0)
+  lw	$t2, 8($a0)
+  lw	$t3, 12($a0)
+  lw	$t4, 16($a0)

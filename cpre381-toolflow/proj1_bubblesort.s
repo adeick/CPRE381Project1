a9581adb0fbc806ddcbe813d5be547b108015265
Rolf Anderson <asaprolfy@gmail.com>
2021-04-05 02:40:14 -0500

    finished, finalizing doc now

--- cpre381-toolflow/proj1_bubblesort.s
+++ cpre381-toolflow/proj1_bubblesort.s
@@ -3,46 +3,45 @@
   #$a1 = n
   
 main:
-  add	$a0, $gp, $zero
-  addi	$a1, $zero, 5
-  addi	$t1, $zero, 1
+  sub	$sp, $sp, 32
+  addi	$a0, $sp, 8
+  addi	$a1, $zero, 20
+  sw	$a1, 4($sp)
+  addi	$t1, $zero, 5
   sw	$t1, 0($a0)
-  addi	$t1, $zero, 2
+  addi	$t1, $zero, 4
   sw	$t1, 4($a0)
   addi	$t1, $zero, 3
   sw	$t1, 8($a0)
-  addi	$t1, $zero, 4
+  addi	$t1, $zero, 2
   sw	$t1, 12($a0)
-  addi	$t1, $zero, 5
+  addi	$t1, $zero, 1
   sw	$t1, 16($a0)
   addi	$t1, $zero, 0
 bubble:
-  addi  $t8, $a0, 0     #ptr:     original pointer to a[]
+  addi  $t8, $sp, 8     #ptr:     original pointer to a[]
   addi  $t7, $zero, 0   #bool:    swapped
-  addi  $t6, $zero, 0   #int:     i
+  addi  $t6, $zero, 4   #int:     i
 
 whileloop:
   slt   $t9, $t6, $a1
-  bne   $t9, 1, whiledone
-  
-swapif:
-	lw	$t0, 0($t8)             #load a = a[i]
-	addi	$t0, $t0, 0
-	lw	$t1, 4($t8)             #load b = a[i+1]
-	addi	$t1, $t1, 0
-	slt	$t2, $t1, $t0           #if a[i+1] < a[i]
-	bne	$t2, 1, else        	#if a[i+1] > a[i], go to else
-	sw	$t1, 0($t8)             #a[i]   = a[i+1]
-	sw	$t0, 4($t8)             #a[i+1] = a[i]
-	addi	$t7, $zero, 1           #set swapped to true
-	j	preloop                 #go to preloop
-else:
-	addi  $t7, $zero, 0
-	j     preloop
+  bne   $t9, 1, isswapped
+  lw	$t0, 0($t8)             #load a = a[i]
+  lw	$t1, 4($t8)             #load b = a[i+1]
+  slt	$t2, $t1, $t0           #if a[i+1] < a[i]
+  bne	$t2, 1, preloop        	#if a[i+1] > a[i], go to else
+  sw	$t1, 0($t8)             #a[i]   = a[i+1]
+  sw	$t0, 4($t8)             #a[i+1] = a[i]
+  addi	$t7, $zero, 1           #set swapped to true
+  j	preloop                 #go to preloop
+
 preloop:
   addi    $t6, $t6, 4           #i++
   addi    $t8, $t8, 4           #a[]++
   j       whileloop             #go back to loop
+isswapped:
+  beq	$t7, 1, bubble
+
 whiledone:
   lw	$t0, 0($a0)
   lw	$t1, 4($a0)
--- cpre381-toolflow/proj1_cf_test.s
+++ cpre381-toolflow/proj1_cf_test.s
@@ -2,49 +2,42 @@ main:
 	addi	$a0, $zero, 0x0
 	addi	$a1, $zero, 0x1
 	addi	$a2, $zero, 0x2
-	addi	$a3, $zero, 0x3
 	jal	stackframetst
 	addi	$s0, $v0, 0
 	j	finish
 
 stackframetst:
-	addi	$sp, $sp, -20
+	addi	$sp, $sp, -16
+	slti	$t4, $a2, 0xF00
+	beq	$t4, 0, tstdone
 	sw	$ra, 0($sp)
 	sw	$a0, 4($sp)
 	sw	$a1, 8($sp)
 	sw	$a2, 12($sp)
-	sw	$a3, 16($sp)
-	addi	$v0, $zero, 0
-	j	stcktstloop
-	
-preloop:
 	addi	$v0, $v0, 1
-	lw	$t3, 16($sp)
-	slti	$t4, $t3, 0xF00
-	bne	$t4, 1, tstdone
 	j	stcktstloop
 	
 stcktstloop:
 	lw	$t0, 4($sp)
 	lw	$t1, 8($sp)
 	lw	$t2, 12($sp)
-	lw	$t3, 16($sp)
-	add	$t3, $t3, $t2
 	add	$t1, $t1, $t0
 	add	$t2, $t2, $t1
-	add	$t3, $t3, $t2
 	addi	$t0, $t0, 1
-	sw	$t0, 4($sp)
-	sw	$t1, 8($sp)
-	sw	$t2, 12($sp)
-	sw	$t3, 16($sp)
-	j	preloop
-	
+	addi	$a2, $t2, 0
+	addi	$a1, $t1, 0
+	addi	$a0, $t0, 0
+	j	stackframetst
 tstdone:
+	addi	$sp, $sp, 16
+	lw	$t0, 4($sp)
+	lw	$t1, 8($sp)
+	lw	$t2, 12($sp)
+	add	$t1, $t1, $t0
+	add	$t2, $t2, $t1
+	add	$v1, $v1, $t2
+	bne	$t0, 1, tstdone
 	lw	$ra, 0($sp)
-	addi	$sp, $sp, 20
 	jr	$ra
 	
 finish:
-	addi	$v0, $zero, 10
-	syscall
